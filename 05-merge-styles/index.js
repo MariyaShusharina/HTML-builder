61d766701253bfcd265d798a28e17aa99015c75e
Mariya Shusharina <mariya.shushar@gmail.com>
2025-01-23 09:23:24 +0300

    feat: do task 5

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,40 @@
+
+const fs = require("fs");
+const fsp = require("fs/promises");
+const path = require("path");
+
+const stylesPath = path.join(__dirname, "styles");
+const distPath = path.join(__dirname, "project-dist", "bundle.css");
+
+const writeStream = fs.createWriteStream(distPath);
+
+let paths = [];
+let exts = [];
+
+let i = 0;
+
+fsp.readdir(stylesPath, {withFileTypes: true})
+  .then((arr) => {
+    arr.forEach(function (elem) {
+      let p = '';
+      p = path.join(stylesPath, elem.name);
+      paths.push(p);
+      exts.push(path.extname(p));
+    });
+    writing();
+  });
+
+function writing() {
+  while (i < paths.length) {
+    if (exts[i] == ".css") {
+      let dat = '';
+      fs.readFile(paths[i], "utf-8", (err, data) => {
+        dat = data;
+        fs.appendFile(distPath, dat, (err) => {});
+        i++;
+        writing();
+      })
+    }
+    i++;
+  }
+}
