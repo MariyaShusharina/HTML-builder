d97db423b9bb9572a9cf0e9373151d8c0dcb1488
Mariya Shusharina <mariya.shushar@gmail.com>
2025-01-23 07:06:38 +0300

    fix: solve task 3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -5,14 +5,60 @@ const path = require("path");
 const { stdout } = require("process");
 const readline = require("readline");
 
+let names = [];
+let exts = [];
+let sizes = [];
+
+let j = 0;
+
 //const readStream = fs.createReadStream("./02-write-file/file.txt");
 
-const dirContents = new Promise ((resolve, reject) => {
-  fsp.readdir("./03-files-in-folder/secret-folder", {withFileTypes: true});
-  resolve();
-  reject();
-});
+fsp.readdir("./03-files-in-folder/secret-folder", {withFileTypes: true})
+  .then((arr) => {
+    //console.log(arr);
+    arr.forEach(function (elem) {
+      if (elem.isFile()) {
+        let p = '';
+        p = "./03-files-in-folder/secret-folder/" + elem.name;
 
+        let nam = '';
+        nam = elem.name;
+        let i = 0;
+        i = nam.indexOf(".");
+        nam = nam.slice(0, i);
+
+        let ext = '';
+        ext = path.extname(p).slice(1);
+
+        //console.log(`${nam} - ${ext} - `);
+
+        names.push(nam);
+        exts.push(ext);
+
+        fsp.stat(p)
+          .then((stats) => {
+            //console.log(stats.size + " bytes");
+            sizes.push(stats.size);
+            console.log(`${names[j]} - ${exts[j]} - ${sizes[j]} bytes`);
+            j++;
+          });
+      }
+    })
+  });
+
+/*
+let p = '';
+p = "./03-files-in-folder/secret-folder/" + elem.name;
+fsp.stat(p)
+  .then((stats) => {
+    console.log(stats.size + " bytes");
+    sizes.push(stats.size);
+  });
+*/
+
+
+
+/*
 dirContents.then(
   function() {
     console.log(dirContents);
@@ -31,6 +77,7 @@ dirContents.then(
   },
   function() {}
 );
+*/
 
 
 
