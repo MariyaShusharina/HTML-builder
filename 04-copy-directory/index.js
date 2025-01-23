4c9478aacfab7ea4c374c52f074c3921f27406bb
Mariya Shusharina <mariya.shushar@gmail.com>
2025-01-23 10:51:16 +0300

    fix: correct script for task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,9 +1,28 @@
 
+const fs = require("fs");
 const fsp = require("fs/promises");
 const path = require("path");
 
 const filesPath = path.join(__dirname, "files");
 const filesCopyPath = path.join(__dirname, "files-copy");
+
+let isCopied = false;
+
+fsp.readdir(__dirname, {withFileTypes: true})
+  .then((arr) => {
+    arr.forEach(function (elem) {
+      console.log(elem);
+      if(elem.isDirectory && elem.name == "files-copy") {
+        fs.rm(filesCopyPath, {recursive: true, force: true}, () => {
+          copyDir();
+          isCopied = true;
+        });
+      }
+    })
+    if(!isCopied) {
+      copyDir();
+    }
+  });
 
 function copyDir() {
 
@@ -22,5 +41,3 @@ function copyDir() {
       })
     });
 }
-
-copyDir();
